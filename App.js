7acc12b417353ff15cdc227f35230eb5e39ac07b
Andre Luiz Parrine Tavares <47765073+JP-Show@users.noreply.github.com>
2022-01-23 00:51:30 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import Animated from 'react-native-reanimated'
+import 'react-native-gesture-handler'
+import React from 'react'
+
+import { NavigationContainer } from '@react-navigation/native'
+
+import Routes from './src/routes'
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  )
+}
+
+export default App
